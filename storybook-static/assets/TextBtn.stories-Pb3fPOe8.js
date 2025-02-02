544a3b89a2b6bf8ff16c151f1d5b9a6f49252f88
524hhhhhh <srose1225@naver.com>
2025-02-03 01:23:27 +0900

    empty commit

--- storybook-static/assets/TextBtn.stories-Pb3fPOe8.js
+++
@@ -1,6 +0,0 @@
-import{j as c}from"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";const n=({text:s,isBlue:a})=>c.jsx("button",{style:{color:a?"blue":"black",border:"none",background:"none",cursor:"pointer"},children:s});n.__docgenInfo={description:"",methods:[],displayName:"TextBtn",props:{text:{required:!0,tsType:{name:"string"},description:""},isBlue:{required:!0,tsType:{name:"boolean"},description:""}}};const p={title:"Buttons/TextBtn",parameters:{layout:"centered"},tags:["autodocs"],component:n,argTypes:{text:{control:"text"},isBlue:{control:"boolean"}}},e={args:{text:"contents",isBlue:!0}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    text: 'contents',
-    isBlue: true
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,p as default};
