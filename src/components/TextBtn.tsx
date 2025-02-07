38ff40995125a9502f8283e15fef089e474aed1f
524hhhhhh <srose1225@naver.com>
2025-02-07 17:45:26 +0900

    add TextBtn Modal stories

--- src/components/TextBtn.tsx
+++ src/components/TextBtn.tsx
@@ -2,25 +2,46 @@ import React from 'react';
 
 interface TextBtnProps {
   text: string;
+  state?: 'Default' | 'Disabled';
+  type?: 'Primary' | 'Secondary';
 }
 
-const TextBtn: React.FC<TextBtnProps> = ({ text }) => {
+const TextBtn: React.FC<TextBtnProps> = ({
+  text,
+  state = 'Default',
+  type = 'Primary',
+}) => {
   return (
     <button
       style={{
         all: 'unset',
-        cursor: 'pointer',
-        color: '#006CFF', // 기본 글자 색상
+        cursor: state === 'Disabled' ? 'not-allowed' : 'pointer',
+        color:
+          state === 'Disabled'
+            ? '#999999'
+            : type === 'Primary'
+            ? '#006CFF'
+            : '#373737', // Disabled 시 컬러 변경
         padding: '4px 8px', // 클릭 영역 확보
         borderRadius: '4px', // 살짝 둥글게 처리
+        opacity: state === 'Disabled' ? 0.5 : 1, // Disabled 상태일 때 투명도 적용
       }}
-      onMouseDown={(e) =>
-        (e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.4)')
-      } // 눌렀을 때 배경색 적용
-      onMouseUp={(e) => (e.currentTarget.style.backgroundColor = 'transparent')} // 뗐을 때 배경색 초기화
-      onMouseLeave={(e) =>
-        (e.currentTarget.style.backgroundColor = 'transparent')
-      } // 마우스가 벗어날 때 초기화
+      disabled={state === 'Disabled'}
+      onMouseDown={(e) => {
+        if (state !== 'Disabled') {
+          e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'; // 눌렀을 때 배경색 적용
+        }
+      }}
+      onMouseUp={(e) => {
+        if (state !== 'Disabled') {
+          e.currentTarget.style.backgroundColor = 'transparent'; // 뗐을 때 배경색 초기화
+        }
+      }}
+      onMouseLeave={(e) => {
+        if (state !== 'Disabled') {
+          e.currentTarget.style.backgroundColor = 'transparent'; // 마우스가 벗어날 때 초기화
+        }
+      }}
     >
       {text}
     </button>
--- src/stories/TextBtn.stories.ts
+++ src/stories/TextBtn.stories.ts
@@ -7,7 +7,6 @@ const meta: Meta<typeof TextBtn> = {
   parameters: {
     layout: 'centered',
   },
-
   argTypes: {
     text: { control: 'text' },
   },
@@ -19,6 +18,25 @@ type Story = StoryObj<typeof TextBtn>;
 
 export const Default: Story = {
   args: {
-    text: 'contents',
+    text: 'content',
+  },
+};
+
+export const Modal: Story = {
+  args: {
+    text: 'content',
+    state: 'Default',
+    type: 'Primary',
+  },
+  argTypes: {
+    state: {
+      control: 'radio',
+      options: ['Default', 'Disabled'],
+    },
+    type: {
+      control: 'select',
+      options: ['Primary', 'Secondary'],
+      if: { arg: 'state', eq: 'Default' }, // 'Default' 상태일 때만 표시
+    },
   },
 };
