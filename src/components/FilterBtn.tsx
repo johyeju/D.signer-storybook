dafc41651123aba4548634bd9366c8f30dd86672
524hhhhhh <srose1225@naver.com>
2025-02-12 10:50:10 +0900

    fix FilterBtn style

--- src/components/FilterBtn.tsx
+++ src/components/FilterBtn.tsx
@@ -1,47 +1,30 @@
 import React from 'react';
 import { icons } from './IIconTypes';
+import './FilterBtn.css';
 
 interface FilterProps {
-	size: 'R' | 'M';
-	text: string;
-	icon?: keyof typeof icons;
+  size: 'R' | 'M';
+  text: string;
+  icon?: keyof typeof icons;
 }
 
 const Filter: React.FC<FilterProps> = ({ size, text, icon }) => {
-	return (
-		<button
-			className={`filter-btn ${
-				size === 'R' ? 'filter-btn--regular' : 'filter-btn--medium'
-			}`}
-			style={{
-				display: 'inline-flex',
-				alignItems: 'center',
-				gap: '8px',
-				padding: '8px 16px',
-				backgroundColor: 'white',
-				color: '#333333', // 텍스트 컬러 변경
-				border: 'none', // 테두리 제거
-			}}
-		>
-			<span className="text-body-13-regular-auto">{text}</span>{' '}
-			{/* 폰트 테일윈드 적용 */}
-			{icon && (
-				<span
-					className="filter-btn__icon"
-					style={{
-						display: 'inline-flex',
-						alignItems: 'center',
-						width: '24px', // 아이콘 사이즈 수정
-						height: '24px', // 아이콘 사이즈 수정
-					}}
-				>
-					{React.cloneElement(icons[icon] as React.ReactElement, {
-						fill: 'currentColor',
-					})}
-				</span>
-			)}
-		</button>
-	);
+  return (
+    <button
+      className={`filter-btn ${
+        size === 'R' ? 'filter-btn--regular' : 'filter-btn--medium'
+      }`}
+    >
+      <span className="text-body-13-regular-auto">{text}</span>
+      {icon && (
+        <span className="filter-btn__icon">
+          {React.cloneElement(icons[icon] as React.ReactElement, {
+            fill: 'currentColor',
+          })}
+        </span>
+      )}
+    </button>
+  );
 };
 
 export default Filter;
