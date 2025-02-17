211562b070395e1e399d54e0f6c28630a9a88039
524hhhhhh <srose1225@naver.com>
2025-02-17 23:23:07 +0900

    update ES Modules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,29 +2,29 @@ import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
-  addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs',
-  ],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-docs',
+	],
 
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
 
-  viteFinal: (config) =>
-    mergeConfig(config, {
-      base: '/D.signer-storybook/',
-    }),
+	viteFinal: (config) =>
+		mergeConfig(config, {
+			base: '/D.signer-storybook/',
+		}),
 
-  docs: {},
+	docs: {},
 
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-  },
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+	},
 };
 export default config;
