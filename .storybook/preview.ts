18e96a77ecf40f4e02e1e7164ee72eb3d0a7b5fd
524hhhhhh <srose1225@naver.com>
2025-02-18 02:16:20 +0900

    add parameters.background

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,16 +2,23 @@ import type { Preview } from '@storybook/react';
 import '../src/index.css'; //Global Css
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		backgrounds: {
+			default: 'light', // 기본 배경색을 dark로 설정
+			values: [
+				{ name: 'light', value: '#ffffff' }, // 밝은 배경
+				{ name: 'dark', value: '#000000' }, // 어두운 배경
+			],
+		},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 
-  tags: ['autodocs'],
+	tags: ['autodocs'],
 };
 
 export default preview;
