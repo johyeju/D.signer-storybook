3053920158727cfe0e1f38a693cb95f162f92e93
524hhhhhh <srose1225@naver.com>
2025-02-18 13:49:01 +0900

    add Modal style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TextBtn from './TextBtn';
 import ListRadio, { ListRadioProps } from './ListRadio';
+import './Modal.css';
 
 export interface ModalProps {
   type?: 'Radio' | 'TimePicker';
@@ -12,28 +13,26 @@ const Modal: React.FC<ModalProps> = ({ title, options }) => {
   const [selectedOption, setSelectedOption] = useState<string>(''); // 하나의 선택 상태만
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-container">
-        {/* 타이틀 */}
-        <h2 className="modal-title">{title}</h2>
+    <div className="modal-container">
+      {/* 타이틀 */}
+      <h2 className="modal-title">{title}</h2>
 
-        {/* 리스트 라디오 */}
-        <div className="modal-options">
-          {options.map((option) => (
-            <ListRadio
-              key={option.title}
-              {...option}
-              size="S"
-              isSelected={selectedOption === option.title} // 선택된 옵션만 활성화
-              onClick={() => setSelectedOption(option.title)} // 중앙에서 선택 상태 관리
-            />
-          ))}
-        </div>
+      {/* 리스트 라디오 */}
+      <div className="modal-options">
+        {options.map((option) => (
+          <ListRadio
+            key={option.title}
+            {...option}
+            size="S"
+            isSelected={selectedOption === option.title} // 선택된 옵션만 활성화
+            onClick={() => setSelectedOption(option.title)} // 중앙에서 선택 상태 관리
+          />
+        ))}
+      </div>
 
-        {/* 하단 버튼 */}
-        <div className="btn-footer">
-          <TextBtn text={'취소'} state="Disabled" type="Primary" />
-        </div>
+      {/* 하단 버튼 */}
+      <div className="btn-footer">
+        <TextBtn text={'취소'} state="Disabled" type="Primary" />
       </div>
     </div>
   );
