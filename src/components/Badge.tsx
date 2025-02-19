ec169d6088faff45f752768a7ad351d0c8d347ea
524hhhhhh <srose1225@naver.com>
2025-02-19 10:18:35 +0900

    add Badge stories & style

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,61 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import './Badge.css';
-import avatar from '../../public/avatars/avatar.svg';
+import { icons } from './IconTransport';
+import avatar from '../../public/badge/avatar.svg';
+import npay from '../../public/badge/npay.svg';
+import talktalk from '../../public/badge/talktalk.svg';
 
 interface BadgeProps {
-	className?: string;
-	type?: 'Artery' | 'WideArea' | 'Default' | 'translucent';
-	congestion?: 'Default' | 'Usually' | 'crowded' | 'last';
-	text: string;
-	label?: string;
-	num?: number;
-	isProfile?: boolean;
+  className?: string;
+  type?: 'Artery' | 'WideArea' | 'Default' | 'translucent';
+  congestion?: 'Default' | 'Usually' | 'crowded' | 'last';
+  text: string;
+  label?: string;
+  num?: number;
+  isProfile?: boolean;
+  isNpay?: boolean;
+  isTalkTalk?: boolean;
+  isIcon?: boolean;
+  iconName?: keyof typeof icons; // icons에서 가져올 아이콘 키
 }
 
 const Badge: React.FC<BadgeProps> = ({
-	className = '',
-	type = 'Default',
-	congestion,
-	text,
-	label: initialLabel,
-	num,
-	isProfile = false,
+  className = '',
+  type = 'Default',
+  congestion,
+  text,
+  label: initialLabel,
+  num,
+  isProfile = false,
+  isNpay = false,
+  isTalkTalk = false,
+  isIcon = false,
+  iconName,
 }) => {
-	// label 상태를 관리하여 congestion 변경 시 자동 업데이트
-	const [label, setLabel] = useState<string | undefined>(initialLabel);
+  // label 상태를 관리하여 congestion 변경 시 자동 업데이트
+  const [label, setLabel] = useState<string | undefined>(initialLabel);
 
-	// congestion 값이 있어야 `label` 업데이트
-	useEffect(() => {
-		if (congestion) {
-			const textMap: Record<
-				'Default' | 'Usually' | 'crowded' | 'last',
-				string
-			> = {
-				Default: '여유',
-				Usually: '보통',
-				crowded: '혼잡',
-				last: '막',
-			};
+  useEffect(() => {
+    if (congestion) {
+      const textMap: Record<
+        'Default' | 'Usually' | 'crowded' | 'last',
+        string
+      > = {
+        Default: '여유',
+        Usually: '보통',
+        crowded: '혼잡',
+        last: '막',
+      };
 
-			setLabel(textMap[congestion]);
-		}
-	}, [congestion]);
+      setLabel(textMap[congestion]);
+    }
+  }, [congestion]);
 
-	return (
-		<span
-			className={`badge ${type ? `badge--${type.toLowerCase()}` : ''} ${
-				congestion ? `badge--${congestion.toLowerCase()}` : ''
-			} ${className}`.trim()}
-			data-story={className}
-		>
-			{/* 프로필 스토리일 때만 avatar.svg 추가 */}
-			{isProfile && <img src={avatar} alt="Profile" className="badge-icon" />}
+  const selectedIcon = isIcon ? icons[iconName || 'bus'] ?? null : null;
 
-			{num && <span className="badge-num">{num}</span>}
-			{text && <span className="badge-text">{text}</span>}
-			{label && <span className="badge-label">{label}</span>}
-		</span>
-	);
+  return (
+    <span
+      className={`badge ${type ? `badge--${type.toLowerCase()}` : ''} ${
+        congestion ? `badge--${congestion.toLowerCase()}` : ''
+      } ${className}`.trim()}
+      data-story={className}
+    >
+      {/* 프로필 스토리일 때만 avatar.svg 추가 */}
+      {isProfile && <img src={avatar} alt="Profile" className="profile-icon" />}
+      {isNpay && <img src={npay} alt="Npay" className="npay-icon" />}
+      {isTalkTalk && (
+        <img src={talktalk} alt="TalkTalk" className="talktalk-icon" />
+      )}
+      {/* isIcon이 true이고 icon 값이 존재하면 버스 SVG 아이콘 표시 */}
+      {selectedIcon && <span className="badge-icon">{selectedIcon}</span>}{' '}
+      {/* 텍스트박스 */}
+      {num && <span className="badge-num">{num}</span>}
+      {text && <span className="badge-text">{text}</span>}
+      {label && <span className="badge-label">{label}</span>}
+    </span>
+  );
 };
 
 export default Badge;
--- src/stories/Badge.stories.ts
+++ src/stories/Badge.stories.ts
@@ -2,32 +2,32 @@ import { Meta, StoryObj } from '@storybook/react';
 import Badge from '../components/Badge';
 
 const meta: Meta<typeof Badge> = {
-	title: 'Content/Badge',
-	component: Badge,
-	parameters: {
-		layout: 'centered',
-	},
+  title: 'Content/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+  },
 
-	argTypes: {
-		type: {
-			control: {
-				type: 'radio',
-				options: ['Artery', 'WideArea', 'Default'],
-			},
-		},
-		congestion: {
-			control: 'radio',
-			options: ['Default', 'Usually', 'crowded', 'last'],
-		},
+  argTypes: {
+    type: {
+      control: {
+        type: 'radio',
+        options: ['Artery', 'WideArea', 'Default'],
+      },
+    },
+    congestion: {
+      control: 'radio',
+      options: ['Default', 'Usually', 'crowded', 'last'],
+    },
 
-		text: {
-			control: 'text',
-		},
-		num: {
-			control: { type: 'number', min: 0, max: 99 }, // 최대 두 자리 숫자
-			defaultValue: 33, // 기본값 33
-		},
-	},
+    text: {
+      control: 'text',
+    },
+    num: {
+      control: { type: 'number', min: 0, max: 99 }, // 최대 두 자리 숫자
+      defaultValue: 33, // 기본값 33
+    },
+  },
 };
 
 export default meta;
@@ -35,111 +35,158 @@ export default meta;
 type Story = StoryObj<typeof Badge>;
 
 export const ImageCount: Story = {
-	args: {
-		num: 33,
-		className: 'image-count-style',
-	},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'text', 'type', 'className', 'label'],
-		},
-	},
+  args: {
+    num: 33,
+    className: 'image-count-style',
+  },
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'text', 'type', 'className', 'label'],
+    },
+  },
+};
+
+export const Npay: Story = {
+  args: {
+    className: 'npay-style',
+    isNpay: true,
+  },
+  parameters: {
+    controls: { disable: true },
+  },
+};
+
+export const talkTalk: Story = {
+  args: {
+    className: 'talktalk-style',
+    isTalkTalk: true,
+  },
+  parameters: {
+    controls: { disable: true },
+  },
 };
 
 export const BusNum: Story = {
-	args: {
-		type: 'Default',
-		text: '1234',
-		className: 'bus-num-style',
-	},
-	argTypes: {
-		type: {
-			control: 'radio',
-			options: ['Artery', 'WideArea', 'Default'],
-		},
-	},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'num', 'className', 'label'],
-		},
-	},
+  args: {
+    type: 'Default',
+    text: '1234',
+    className: 'bus-num-style',
+  },
+  argTypes: {
+    type: {
+      control: 'radio',
+      options: ['Artery', 'WideArea', 'Default'],
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'num', 'className', 'label'],
+    },
+  },
+};
+
+export const BusBadge: Story = {
+  args: {
+    text: 'NNNN',
+    className: 'bus-badge-style',
+    isIcon: true,
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'congestion',
+        'num',
+        'type',
+        'className',
+        'label',
+        'isProfile',
+        'isIcon',
+      ],
+    },
+    backgrounds: {
+      default: 'dark', //백그라운드 추가
+    },
+  },
 };
 
 export const RemainingSeats: Story = {
-	args: {
-		num: 33,
-		text: '석',
-		className: 'remaining-seats-style',
-	},
-	argTypes: {},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'text', 'type', 'className', 'label'],
-		},
-	},
+  args: {
+    num: 33,
+    text: '석',
+    className: 'remaining-seats-style',
+  },
+  argTypes: {},
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'text', 'type', 'className', 'label'],
+    },
+  },
 };
 
 export const StopLocation: Story = {
-	args: {
-		congestion: 'Default',
-		className: 'stop-location-style',
-	},
-	argTypes: {
-		congestion: {
-			control: 'radio',
-			options: ['Default', 'Usually', 'crowded', 'last'],
-		},
-	},
-	parameters: {
-		controls: {
-			exclude: ['type', 'num', 'className', 'text'],
-		},
-	},
+  args: {
+    congestion: 'Default',
+    className: 'stop-location-style',
+  },
+  argTypes: {
+    congestion: {
+      control: 'radio',
+      options: ['Default', 'Usually', 'crowded', 'last'],
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['type', 'num', 'className', 'text'],
+    },
+  },
 };
 export const Village: Story = {
-	args: {
-		text: '마을',
-		type: 'Default',
-		className: 'village-style',
-	},
-	argTypes: {
-		type: {
-			control: 'radio',
-			options: ['Default', 'translucent'],
-		},
-	},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'text', 'num', 'className', 'label'],
-		},
-		backgrounds: {
-			default: 'dark', //백그라운드 추가
-		},
-	},
+  args: {
+    text: '마을',
+    type: 'Default',
+    className: 'village-style',
+  },
+  argTypes: {
+    type: {
+      control: 'radio',
+      options: ['Default', 'translucent'],
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'text', 'num', 'className', 'label'],
+    },
+    backgrounds: {
+      default: 'dark', //백그라운드 추가
+    },
+  },
 };
 
 export const TransportState: Story = {
-	args: {
-		text: '중앙',
-		className: 'transport-state-style',
-	},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'text', 'num', 'type', 'className', 'label'],
-			//controls패널 false처리
-		},
-	},
+  args: {
+    text: '중앙',
+    className: 'transport-state-style',
+  },
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'text', 'num', 'type', 'className', 'label'],
+      //controls패널 false처리
+    },
+  },
 };
 
 export const Profile: Story = {
-	args: {
-		text: 'nickname',
-		className: 'profile-style',
-		isProfile: true,
-	},
-	parameters: {
-		controls: {
-			exclude: ['congestion', 'num', 'type', 'className', 'label', 'isProfile'],
-		},
-	},
+  args: {
+    text: 'nickname',
+    className: 'profile-style',
+    isProfile: true,
+  },
+  parameters: {
+    controls: {
+      exclude: ['congestion', 'num', 'type', 'className', 'label', 'isProfile'],
+    },
+    backgrounds: {
+      default: 'dark', //백그라운드 추가
+    },
+  },
 };
